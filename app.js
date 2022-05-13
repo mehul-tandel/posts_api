4fa1b8b462e55b0d3e3b5ae9435f425073fcf7f0
Mehul Tandel <mehultandel.tech@gmail.com>
2022-05-13 15:23:59 +0530

    created post model and connected to mogodb

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const keys = require('./keys');
 const feedRoutes = require('./routes/feed');
 
 const app = express();
@@ -17,4 +19,7 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose.connect('mongodb+srv://mehul_tandel31:'+keys.cluster0+'@cluster0.mr78j.mongodb.net/messages?retryWrites=true&w=majority')
+.then(result => {
+    app.listen(8080)
+}).catch(err => console.log(err));
