5841267f9a3144411f3b02b2e4a7b118c0b20036
Mehul Tandel <mehultandel.tech@gmail.com>
2022-05-14 13:31:24 +0530

    image uploading added using multer

--- app.js
+++ app.js
@@ -1,35 +1,72 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
 
-const keys = require('./keys');
-const feedRoutes = require('./routes/feed');
+const keys = require("./keys");
+const feedRoutes = require("./routes/feed");
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString().replace(/:/g, '-') + "-" + file.originalname);
+    },
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpg" ||
+        file.mimetype === "image/jpeg"
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
 app.use(bodyParser.json());
-app.use('/images', express.static(path.join(__dirname,'images')));
+app.use(
+    multer({
+        storage: fileStorage,
+        fileFilter: fileFilter,
+    }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 // Allowing CORS(Cross Origin Resource Sharing)
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+    );
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
     res.status(status).json({ message: message });
-})
+});
 
-mongoose.connect('mongodb+srv://mehul_tandel31:'+keys.cluster0+'@cluster0.mr78j.mongodb.net/messages?retryWrites=true&w=majority')
-.then(result => {
-    app.listen(8080)
-}).catch(err => console.log(err));
+mongoose
+    .connect(
+        "mongodb+srv://mehul_tandel31:" +
+        keys.cluster0 +
+        "@cluster0.mr78j.mongodb.net/messages?retryWrites=true&w=majority"
+    )
+    .then((result) => {
+        app.listen(8080);
+    })
+    .catch((err) => console.log(err));
