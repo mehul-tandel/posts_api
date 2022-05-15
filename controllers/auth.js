3314031f6c442061bcc7ce0d1c052de5148c4918
Mehul Tandel <mehultandel.tech@gmail.com>
2022-05-15 13:22:24 +0530

    user authentication using jwt during login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -36,5 +37,44 @@ exports.signup = (req, res, next) => {
             }
             next();
         });
+};
 
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                const error = new Error('User with this email does not exist.');
+                error.statusCode = 401;
+                throw error;
+            }
+            loadedUser = user;
+            return bcrypt.compare(password, user.password);
+        })
+        .then(isEqual => {
+            if (!isEqual) {
+                const error = new Error('Wrong Password!');
+                error.statusCode = 401;
+                throw error;
+            }
+            const token = jwt.sign({
+                email: loadedUser.email,
+                userId: loadedUser._id.toString()
+            },
+                'longsecretstring',
+                { expiresIn: '1h' }
+            );
+            res.status(200).json({
+                token: token,
+                userId: loadedUser._id.toString()
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next();
+        })
 }
--- routes/auth.js
+++ routes/auth.js
@@ -11,15 +11,17 @@ router.put('/signup', [
         .isEmail()
         .withMessage('Enter a valid email.')
         .custom((value, { req }) => {
-            return User.findOne({email: value}).then(userDoc => {
+            return User.findOne({ email: value }).then(userDoc => {
                 if (userDoc) {
                     return Promise.reject('Email already exists.');
                 }
             })
         })
         .normalizeEmail(),
-    body('password').trim().isLength({min: 5}),
+    body('password').trim().isLength({ min: 5 }),
     body('name').trim().not().isEmpty()
 ], authController.signup);
+
+router.post('/login', authController.login);
 
 module.exports = router;
