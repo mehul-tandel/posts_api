994ea80e5f1a4ab660c21ae586aa34d36ceffee9
Mehul Tandel <mehultandel.tech@gmail.com>
2022-05-15 12:50:12 +0530

    created user model

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const multer = require("multer");
 
 const keys = require("./keys");
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 
 const app = express();
 
@@ -52,12 +53,14 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
-    res.status(status).json({ message: message });
+    const data = error.data;
+    res.status(status).json({ message: message, data: data });
 });
 
 mongoose
