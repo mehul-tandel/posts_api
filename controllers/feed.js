c61c93b8d3ed21dcfebbe0f244fd8300c457adae
Mehul Tandel <mehultandel.tech@gmail.com>
2022-05-15 15:29:28 +0530

    user authorization check before edit/delete posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -127,6 +127,11 @@ exports.updatePost = (req,res,next) => {
                 error.statusCode = 404;
                 throw error;
             }
+            if (post.creator.toString() !== req.userId) {
+                const error = new Error('Not Authorized.');
+                error.statusCode = 403;
+                throw error;
+            }
             if (imageUrl !== post.imageUrl) {
                 clearImage(post.imageUrl);
             }
@@ -156,6 +161,11 @@ exports.deletePost = (req,res,next) => {
             if (!post) {
                 const error = new Error('Could not find the post');
                 error.statusCode = 404;
+                throw error;
+            }
+            if (post.creator.toString() !== req.userId) {
+                const error = new Error('Not Authorized.');
+                error.statusCode = 403;
                 throw error;
             }
             //Check logged in user
