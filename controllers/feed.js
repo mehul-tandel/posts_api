04cfa6dca97d53d573c657f54f26dbe803b06417
Mehul Tandel <mehultandel.tech@gmail.com>
2022-05-15 15:07:25 +0530

    connected user and its posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -48,21 +49,29 @@ exports.createPost = (req, res, next) => {
     const imageUrl = req.file.path;
     const title = req.body.title;
     const content = req.body.content;
+    let creator;
     const post = new Post({
         title: title,
         content: content,
         imageUrl: imageUrl,
-        creator: {
-            name: 'Mehul'
-        },
+        creator: req.userId,
     });
     post.save().then(result => {
-        console.log(result);
+        return User.findById(req.userId);
+    })
+    .then(user =>{
+        creator = user;
+        user.posts.push(post);
+        return user.save();
+    })
+    .then(result => {
         res.status(201).json({
             message: 'Post created successfully!',
-            post: result
-        });
-    }).catch(err => {
+            post: post,
+            creator: {_id: creator._id, name: creator.name}
+        })
+    })
+    .catch(err => {
         if (!err.statusCode) {
             err.statusCode = 500;
         }
--- routes/feed.js
+++ routes/feed.js
@@ -2,13 +2,14 @@ const express = require('express');
 const { body } = require('express-validator');
 
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth , feedController.getPosts);
 
-router.post('/post',[
+router.post('/post', isAuth, [
     body('title').trim().isLength({min: 5}),
     body('content').trim().isLength({min: 5})
 ] , feedController.createPost);
@@ -20,6 +21,6 @@ router.put('post/:postId',[
     body('content').trim().isLength({min: 5})
 ] , feedController.updatePost);
 
-router.delete('/post/:postId', feedController.deletePost);
+router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
